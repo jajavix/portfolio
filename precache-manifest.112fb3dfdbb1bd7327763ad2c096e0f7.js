f37ec18b7ac6723a9ecd29619118acce703222ec
maria <mariajvillarin@gmail.com>
2023-01-26 12:54:04 -0800

    Updates

---
+++ precache-manifest.112fb3dfdbb1bd7327763ad2c096e0f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab37114a56044c61515c",
+    "url": "/portfolio/static/css/main.913f0a6b.chunk.css"
+  },
+  {
+    "revision": "ab37114a56044c61515c",
+    "url": "/portfolio/static/js/main.ab37114a.chunk.js"
+  },
+  {
+    "revision": "d01854737b9ab9e01fa9",
+    "url": "/portfolio/static/js/1.d0185473.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "23cd758fc5b7006984755bca2f82824d",
+    "url": "/portfolio/static/media/Maria J Villarin_Resume.23cd758f.pdf"
+  },
+  {
+    "revision": "7ea9a7041bea54f8465e7a723c90767e",
+    "url": "/portfolio/static/media/Hakone_ne.7ea9a704.png"
+  },
+  {
+    "revision": "6ad3a7ae55425a97a24ace11fc1c6fb3",
+    "url": "/portfolio/static/media/down-arrow.6ad3a7ae.svg"
+  },
+  {
+    "revision": "709f3e2e5fde467a4b08898f7bc80bc8",
+    "url": "/portfolio/static/media/Profile-Picture.709f3e2e.jpg"
+  },
+  {
+    "revision": "5665352a171ea64cd59865cb25944ec8",
+    "url": "/portfolio/static/media/LandingPageDesign.5665352a.png"
+  },
+  {
+    "revision": "02f1c315e6aee9a9c02a6f56f8b55aab",
+    "url": "/portfolio/static/media/BookStore.02f1c315.png"
+  },
+  {
+    "revision": "c79dc980851c8fc49c48b5b4f550a5d0",
+    "url": "/portfolio/index.html"
+  }
+];
